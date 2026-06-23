9e26031e36a89a3299592f45d846306c3debb74a
devmilon923 <dev.milon923@gmail.com>
2026-06-23 22:10:39 +0600

    logout and renew token codded fully sync with backend

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,11 @@ import { Button } from "@/components/ui/button";
 import api from "@/hooks/axios";
 
 export default function Home() {
+  const backendURL = process.env.NEXT_PUBLIC_Backend_URL;
   const handleLogout = async () => {
     try {
-      await api.get("http://localhost:3000/google/logout");
-    } catch (error) {
-      console.log(error);
-    }
+      await api.get(backendURL + "/logout");
+    } catch (error: any) {}
   };
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
--- hooks/axios.ts
+++ hooks/axios.ts
@@ -9,7 +9,24 @@ const api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    console.log("Error from interceptors", error);
+    const originalRequest = error.config;
+    console.log(error.response?.status);
+    if (
+      (!originalRequest._retry && error.response?.status === 401) ||
+      error.response?.status === 403
+    ) {
+      originalRequest._retry = true;
+      try {
+        await axios.get(backendURL + "/renew", {
+          withCredentials: true,
+        });
+        return api(originalRequest);
+      } catch (error) {
+        console.log(error);
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
   },
 );
 export default api;
