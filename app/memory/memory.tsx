e1bbbe8f755ea59cb0e1a3e32417c1dc6ad973af
devmilon923 <dev.milon923@gmail.com>
2026-06-25 22:20:53 +0600

    added auth context provider

---
+++ app/memory/memory.tsx
@@ -0,0 +1,33 @@
+"use client";
+import {
+  createContext,
+  ReactNode,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+interface UserType {
+  name: string;
+  email: string;
+  oauthid: string;
+  id: string;
+}
+
+interface ContextPayload {
+  user: UserType | null;
+  setUser: Dispatch<SetStateAction<UserType | null>>;
+}
+
+// Initialize the context
+export const AuthContext = createContext<ContextPayload | null>(null);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<UserType | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
