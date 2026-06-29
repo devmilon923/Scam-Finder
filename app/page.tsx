a57e6f460ed70a0221717d5afcf2b672fd11be49
devmilon923 <dev.milon923@gmail.com>
2026-06-29 21:07:21 +0600

    Revert "added intial design for store card"
    This reverts commit 6cc873f5bada360e637ddc90181e1b55fb48d07f.

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,22 @@
 "use client";
-
-import StoreCard from "@/components/custom/store-card";
 import { Button } from "@/components/ui/button";
 import api from "@/hooks/axios";
 import { useAuth } from "@/providers/memory";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const { logout } = useAuth();
+  const router = useRouter();
+
+  const handleProfile = async () => {
+    return router.push("/user/profile");
+  };
+  const handleAuth = async () => {
+    return router.push("/auth");
+  };
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <StoreCard
-        name="Maison & Co."
-        category="Fashion & Apparel"
-        logo="https://img.magnific.com/premium-vector/creative-elegant-abstract-minimalistic-logo-design-vector-any-brand-company_1253202-137644.jpg"
-        platform="facebook"
-        allegation="Money Scam"
-        description="Multiple customers reported payment issues and delayed refunds. Click to see full details."
-        joinedDate="Jan 15, 2024"
-        location="New York, USA"
-      />
-      {/* <Button onClick={handleProfile}>Profile</Button>
+      <Button onClick={handleProfile}>Profile</Button>
       <Button
         variant={"secondary"}
         onClick={() => {
@@ -29,7 +26,7 @@ export default function Home() {
       >
         Logout
       </Button>
-      <Button onClick={handleAuth}>Auth</Button> */}
+      <Button onClick={handleAuth}>Auth</Button>
     </div>
   );
 }
